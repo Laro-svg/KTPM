7c5c8b1ca93e0fbf290ef2060c3e3268c41b5114
Laro-svg <vytri1992@gmail.com>
2021-04-12 11:06:33 +0700

    initial

---
+++ modules/content/giothieu.php
@@ -0,0 +1,42 @@
+<div class="col-xs-12 col-sm-8 col-md-12 ">
+    <div class="about-section-box">
+		<div class="container">
+			<h1 style="margin-left: 400px; font-size: 60px; margin-top: -50px;">DỊCH VỤ BẢO HÀNH</h1>
+			<div class="row" style="margin-top: 30px;">
+				<div class="col-lg-6 col-md-6">
+					<img src="images/vs_1.jpg" alt="" class="img-fluid">
+				</div>
+				<div class="col-lg-6 col-md-6 text-center">
+					<div class="inner-column">
+						<h1>Vệ sinh máy lạnh tại nhà </h1>
+						<a class="btn btn-lg btn-circle btn-outline-new-white" href="vesinh.php">Xem chi tiết</a>
+					</div>
+				</div>
+				
+			</div>
+			<div class="row" style="margin-top: 30px;">
+				
+				<div class="col-lg-6 col-md-6 text-center" >
+					<div class="inner-column">
+						<h1>Sửa chữa máy lạnh tại nhà </h1>
+						<a class="btn btn-lg btn-circle btn-outline-new-white" href="suachua.php">Xem chi tiết</a>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6">
+					<img src="images/sc_1.jpg" alt="" class="img-fluid">
+				</div>
+			</div>
+			<div class="row" style="margin-top: 30px;">
+				<div class="col-lg-6 col-md-6"  >
+					<img src="images/ld_1.jpg" alt="" class="img-fluid">
+				</div>
+				<div class="col-lg-6 col-md-6 text-center">
+					<div class="inner-column">
+						<h1>Lắp đặt máy lạnh tại nhà </h1>
+						<a class="btn btn-lg btn-circle btn-outline-new-white" href="../lapdat.php">Xem chi tiết</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
